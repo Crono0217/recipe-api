956043a4423f958292aea08ca98732d555c8bce4
Crono0217 <os99@njit.edu>
2026-01-28 23:24:21 -0500

    added input validation to create and update schemas

--- app/routes/recipes.py
+++ app/routes/recipes.py
@@ -16,32 +16,37 @@ def get_db_session():
 
 #create route
 @router.post("/recipes")
-def route_create_recipe(title: str, 
-                        author_name:str, 
-                        ingredients:str, 
-                        cook_time:str, 
-                        db: Session = Depends(get_db_session)):
+def route_create_recipe(
+    title: str, 
+    author_name:str, 
+    ingredients:str, 
+    cook_time:str, 
+    db: Session = Depends(get_db_session)):
     return create_recipe(db, title, author_name, ingredients, cook_time)
 
 #read route
 @router.get("/recipe/{recipe_id}")
-def route_get_recipe(recipe_id: int, 
-                     db: Session =  Depends(get_db_session)):
+def route_get_recipe(
+    recipe_id: int, 
+    db: Session =  Depends(get_db_session)):
     return get_recipe(db, recipe_id)
 #read all route
 @router.get("/recipes")
-def route_get_recipes(db:Session = Depends(get_db_session)):
+def route_get_recipes(
+    db:Session = Depends(get_db_session)):
     return get_recipes(db)
 
 #update route
 @router.patch("/recipe/{recipe_id}")
-def route_update_recipe(recipe_id: int, 
-                        recipe: RecipeUpdate,
-                        db: Session = Depends(get_db_session)):
+def route_update_recipe(
+    recipe_id: int, 
+    recipe: RecipeUpdate,
+    db: Session = Depends(get_db_session)):
     return update_recipe(db, recipe_id, recipe)
     
 #delete route
 @router.delete("/recipe/{recipe_id}")
-def route_delete_recipe(recipe_id: int, 
-                        db: Session = Depends(get_db_session)):
+def route_delete_recipe(
+    recipe_id: int, 
+    db: Session = Depends(get_db_session)):
     return delete_recipe(db, recipe_id)
